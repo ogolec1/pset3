97d010f6cbbaa83943f806a79df397144b9e3a66
ogolec1 <55405269+ogolec1@users.noreply.github.com>
2020-09-27 21:03:11 -0400

    Update Exercise4.java

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -9,38 +9,24 @@ import static java.lang.Float.parseFloat;
 public class Exercise4 {
 
     public static void main(String[] args) {
-
         Scanner scannerA = new Scanner(System.in);
-
         System.out.print("Height : ");
-
         String heightString = scannerA.nextLine();
-
         float height = parseFloat(heightString);
-
         Scanner scannerB = new Scanner(System.in);
 
         System.out.print("Width : ");
-
         String widthString = scannerB.nextLine();
-
         float width = parseFloat(widthString);
-
+        
         double cmHeight = height * 2.54;
-
         double cmWidth = width * 2.54;
 
         double perimeter = 2 * (cmHeight + cmWidth);
 
-
-
         String pattern = "###,##0.00";
-
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
-
         String finalPerimeter = decimalFormat.format(perimeter);
-
-
 
         System.out.print("\n" + finalPerimeter + " centimeters." + "\n");
 
