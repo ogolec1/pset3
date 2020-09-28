7e27463fac39a0873f3bc6fb7c7eee65b9756ca1
ogolec1 <55405269+ogolec1@users.noreply.github.com>
2020-09-27 20:51:41 -0400

    Update Exercise10.java

--- src/Exercise10.java
+++ src/Exercise10.java
@@ -1,1 +1,33 @@
+import java.text.DecimalFormat;
 
+import java.util.Scanner;
+
+
+
+public class Exercise10 {
+
+    public static void main(String[] args) {
+
+        Scanner scannerA = new Scanner(System.in);
+        System.out.print("Fahrenheit : ");
+        String fahrString = scannerA.nextLine();
+        double fahrenheit = Double.parseDouble(fahrString);
+
+
+
+        double celsius = (fahrenheit - 32) * 5/9;
+
+        double kelvin = ((fahrenheit - 32) * 5/9) + 273.15;
+
+
+        String pattern = "###,##0.00";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+      
+        String finalKelvin = decimalFormat.format(kelvin);
+        String finalCelsius = decimalFormat.format(celsius);
+
+
+        System.out.print("\nCelsius : " + finalCelsius + "\nKelvin  : " + finalKelvin + "\n");
+      
+    }
+}
