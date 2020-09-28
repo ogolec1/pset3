386ee6dedc6400836c31b1e7ec6b09303d4a89e1
ogolec1 <55405269+ogolec1@users.noreply.github.com>
2020-09-27 20:45:49 -0400

    Update Exercise8.java

--- src/Exercise8.java
+++ src/Exercise8.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+public class Exercise8 {
+    public static void main(String[] args) {
+        Scanner scannerA = new Scanner(System.in);
+        System.out.print("Students  : ");
+
+      
+        String studentString = scannerA.nextLine();
+        int students = Integer.parseInt(studentString);
+
+
+        Scanner scannerB = new Scanner(System.in);
+        System.out.print("Teachers  : ");
+        String teacherString = scannerB.nextLine();
+        int teachers = Integer.parseInt(teacherString);
+
+        Scanner scannerC = new Scanner(System.in);
+        System.out.print("Capacity  : ");
+        String capacityString = scannerC.nextLine();
+        int capacity = Integer.parseInt(capacityString);
+
+        int totalPeople = students + teachers;
+        int buses = (totalPeople / capacity) + 1;
+        int overflow = totalPeople % capacity;
+
+
+        System.out.print("\nBuses required      : " + buses + "\nOverflow passengers : " + overflow + "\n");
+
+    }
+}
